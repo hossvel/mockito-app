f182a373837c63026868109199aaee8c444656b4
devhoss <velasco2288@gmail.com>
2023-09-07 10:40:05 -0500

    mockeando la clase repository implements

---
+++ src/test/java/org/hossvel/services/Datos.java
@@ -0,0 +1,22 @@
+package org.hossvel.services;
+
+import org.hossvel.models.Examen;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Datos {
+    public final static List<Examen> EXAMENES =
+            List.of(
+                    new Examen(5L, "Matematica"),
+                    new Examen(6L, "Lenguaje"),
+                    new Examen(7L, "Historia"),
+                    new Examen(8L, "Ingles"),
+                    new Examen(9L, "Algebra")
+
+            );
+
+    public final static List<String> PREGUNTAS = Arrays.asList("aritmética","integrales",
+            "derivadas", "trigonometría", "geometría");
+    public final static Examen EXAMEN = new Examen(null, "Física");
+}
--- src/test/java/org/hossvel/services/ExamenServiceImplTest.java
+++ src/test/java/org/hossvel/services/ExamenServiceImplTest.java
@@ -1,7 +1,6 @@
 package org.hossvel.services;
 
 import org.hossvel.models.Examen;
-import org.hossvel.repository.ExamenRepositoryImpl;
 import org.hossvel.repository.IExamenRepository;
 import org.hossvel.service.ExamenServiceImpl;
 import org.hossvel.service.IExamenService;
@@ -15,8 +14,10 @@ public class ExamenServiceImplTest {
     @Test
     void findExamenPorNombre() {
 
-        IExamenRepository iexamenRepository = new ExamenRepositoryImpl();
+        IExamenRepository iexamenRepository = mock(IExamenRepository.class);
         IExamenService iexamenService = new ExamenServiceImpl(iexamenRepository);
+
+        when(iexamenRepository.findAll()).thenReturn(Datos.EXAMENES);
 
         Examen examen = iexamenService.findExamenPorNombre("Lenguaje");
         assertNotNull(examen);
